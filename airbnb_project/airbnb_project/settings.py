882d89acb6aba1aae0bfebc0fedfb6ac924eaef2
Nam Anh Mai <maithenamanh@gmail.com>
2024-05-22 12:06:59 -0700

    Added secret key and debug (must be false in production) key to env

--- airbnb_project/airbnb_project/settings.py
+++ airbnb_project/airbnb_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-53yrlz+hs^cgts5uogf_b3!jgkky(d_n_(()y=c7qm))n@urm$'
+load_dotenv(find_dotenv())
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DJANGO_DEBUG'] != 'False'
 
 ALLOWED_HOSTS = []
 
