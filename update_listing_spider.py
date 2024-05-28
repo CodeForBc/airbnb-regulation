7ecd2bdd156978227354b825ce4e8ac35f6f6b1c
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-05-28 12:48:48 -0700

    add scrape file and explanation

---
+++ update_listing_spider.py
@@ -0,0 +1,319 @@
+import csv
+import json
+import random
+
+import scrapy
+from scrapy import Request
+from scrapy.crawler import CrawlerRunner, CrawlerProcess
+from airbnb_listings.items import AirBnBListingItem, AirBnbId, ExpandedAirBnBListingItem
+from scrapy.http import Response
+import base64
+
+
+# The tag used to identify the specific script data within the Airbnb HTML page.
+SCRIPT_TAG = "data-deferred-state-0"
+
+# API endpoint for retrieving detailed information about specific Airbnb listings.
+# The URL requires an Airbnb listing ID to be inserted into the placeholder.
+API_CALL = "https://www.airbnb.ca/api/v3/StaysPdpSections/08e3ad2e3d75c9bede923485718ff2e7f6efe2ca1febb5192d78c51e17e8b4ca?operationName=StaysPdpSections&locale=en-CA&currency=CAD&variables=%7B%22id%22%3A%22{}%22%2C%22pdpSectionsRequest%22%3A%7B%22adults%22%3A%221%22%2C%22layouts%22%3A%5B%22SINGLE_COLUMN%22%5D%7D%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%2237d7cbb631196506c3990783fe194d81432d0fbf7362c668e547bb6475e71b37%22%7D%7D"
+
+# API key used for authentication when making API calls to Airbnb's services.
+API_KEY = "d306zoyjsyarp7ifhu67rjxn52tv0t20"
+
+# Zoom level parameter used in the map URL templates to define the zoom level of the map.
+ZOOM_LEVEL = 15.4
+
+# A specific map URL for searching Airbnb listings in Vancouver with predefined coordinates and search parameters.
+MAP_URL = "https://www.airbnb.ca/s/Vancouver--Canada/homes?drawer_open=true&tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&query=Vancouver%2C%20Canada&place_id=ChIJs0-pQ_FzhlQRi_OBm-qWkbs&flexible_trip_lengths%5B%5D=one_week&monthly_start_date=2024-05-027&monthly_length=3&monthly_end_date=2024-08-01&search_mode=regular_search&price_filter_input_type=0&channel=EXPLORE&ne_lat=49.29513742488498&ne_lng=-123.10579065845081&sw_lat=49.27164621670274&sw_lng=-123.14562551992225&search_by_map=true&zoom_level=13.966716075400992&map_toggle=true&search_type=user_map_move&price_filter_num_nights=5&zoom=13.966716075400992"
+
+# URL template for searching Airbnb listings in Vancouver with placeholders for coordinates and zoom level.
+# This template focuses on homes available for a one-week stay within specific dates.
+MAP_URL_TEMPLATE1 = "https://www.airbnb.ca/s/Vancouver--Canada/homes?drawer_open=true&tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&query=Vancouver%2C%20Canada&place_id=ChIJs0-pQ_FzhlQRi_OBm-qWkbs&flexible_trip_lengths%5B%5D=one_week&monthly_start_date=2024-05-27&monthly_length=2&monthly_end_date=2024-07-01&search_mode=regular_search&price_filter_input_type=0&channel=EXPLORE&ne_lat={}&ne_lng={}&sw_lat={}&sw_lng={}&search_by_map=true&zoom_level={}&map_toggle=true&search_type=user_map_move&price_filter_num_nights=5&zoom={}"
+
+# URL template similar to the previous one but includes additional flexible trip date options (May, June, July, August) and focuses on weekend trips.
+MAP_URL_TEMPLATE2 = "https://www.airbnb.ca/s/Vancouver--Canada/homes?drawer_open=true&tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&query=Vancouver%2C%20Canada&place_id=ChIJs0-pQ_FzhlQRi_OBm-qWkbs&flexible_trip_lengths%5B%5D=one_week&monthly_start_date=2024-05-27&monthly_length=2&monthly_end_date=2024-07-01&search_mode=regular_search&price_filter_input_type=0&channel=EXPLORE&ne_lat={}&ne_lng={}&sw_lat={}&sw_lng={}&search_by_map=true&zoom_level={}&map_toggle=true&search_type=user_map_move&price_filter_num_nights=5&zoom={}&flexible_trip_dates%5B%5D=august&flexible_trip_dates%5B%5D=july&flexible_trip_dates%5B%5D=june&flexible_trip_dates%5B%5D=may&flexible_trip_lengths%5B%5D=weekend_trip"
+
+# URL template for searching Airbnb listings in Vancouver with placeholders for coordinates and zoom level.
+# This template includes a variety of search parameters including flexible trip dates and lengths.
+MAP_URL_TEMPLATE = "https://www.airbnb.ca/s/Vancouver--Canada/homes?drawer_open=true&tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&query=Vancouver%2C%20Canada&place_id=ChIJs0-pQ_FzhlQRi_OBm-qWkbs&flexible_trip_lengths%5B%5D=one_week&monthly_start_date=2024-05-27&monthly_length=2&monthly_end_date=2024-07-01&search_mode=regular_search&price_filter_input_type=0&channel=EXPLORE&ne_lat={}&ne_lng={}&sw_lat={}&sw_lng={}&search_by_map=true&zoom_level={}&map_toggle=true&search_type=user_map_move&price_filter_num_nights=5&zoom={}&flexible_trip_dates%5B%5D=august&flexible_trip_dates%5B%5D=july&flexible_trip_dates%5B%5D=june&flexible_trip_dates%5B%5D=may&lexible_trip_lengths%5B%5D=one_month&adults=1&children=1&infants=1&pets=1&checkin=2024-05-17&checkout=2024-06-10&flexible_date_search_filter_type=3"
+
+# URL template for searching Airbnb listings in Vancouver for a month-long stay.
+# Includes placeholders for coordinates and zoom level, as well as a focus on monthly stays.
+MAP_URL_TEMPLATE3 = "https://www.airbnb.ca/s/Vancouver--Canada/homes?drawer_open=true&tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&query=Vancouver%2C%20Canada&place_id=ChIJs0-pQ_FzhlQRi_OBm-qWkbs&flexible_trip_lengths%5B%5D=one_month&monthly_start_date=2024-05-27&monthly_length=2&monthly_end_date=2024-07-01&search_mode=regular_search&price_filter_input_type=0&channel=EXPLORE&ne_lat={}&ne_lng={}&sw_lat={}&sw_lng={}&search_by_map=true&zoom_level={}&map_toggle=true&search_type=user_map_move&price_filter_num_nights=28&zoom={}"
+
+# A list of all URL templates for map-based searches, combining different search parameters and flexible options.
+MAP_URL_TEMPLATES = [MAP_URL_TEMPLATE1, MAP_URL_TEMPLATE2, MAP_URL_TEMPLATE, MAP_URL_TEMPLATE3]
+
+
+def n(s):
+    """
+    Encode the input string `s` into bytes, then encode it in base64, and decode it back to a UTF-8 string.
+
+    :param s: The input string to be encoded.
+    :return: The base64 encoded string.
+    """
+    return base64.b64encode(s.encode('utf-8')).decode('utf-8')
+
+
+from urllib.parse import quote
+
+
+def t(n, t):
+    """
+    Encode the second string `t` using URL encoding, replace spaces with plus signs, and return the combined format.
+
+    :param n: The first part of the string to be combined.
+    :param t: The string to be URL encoded and combined.
+    :return: A formatted string combining `n` and the encoded version of `t`.
+    """
+    encoded_t = quote(t, safe='').replace('%20', '+').replace('(', '%28').replace(')', '%29')
+    return f"{n}:{encoded_t}"
+
+
+class UpdateListingSpider(scrapy.Spider):
+    """
+    Spider for scraping Airbnb listings.
+    """
+    name = "update_listing_spider"
+    allowed_domains = ['airbnb.ca']
+    start_urls = [MAP_URL]
+    next_page_cursors: [str] = None
+    total_listings = 0
+    ALREADY_SCRAPED_LISTINGS = set()
+
+    def start_requests(self):
+        """
+       Generates initial requests for scraping Airbnb listings using coordinates from a JSON file.
+
+       This function performs the following steps:
+       1. Reads already scraped Airbnb listing IDs from a CSV file and stores them in a set.
+       2. Reads coordinates from a JSON file and shuffles them.
+       3. Constructs and returns a list of initial Scrapy requests using template URLs and the read coordinates.
+
+       :return: List of Scrapy FormRequest objects initialized with map coordinates for Vancouver.
+        """
+        file_name = self.settings.get("CSV_STORE_FILE_NAME")
+        try:
+            with open(
+                    f"{file_name}",
+                    "r",
+                    encoding="utf8") as file:
+                read_csv_file = csv.DictReader(file)
+                for row in read_csv_file:
+                    row: dict
+                    UpdateListingSpider.ALREADY_SCRAPED_LISTINGS.add(row['airbnb_listing_id'])
+                print("ids", UpdateListingSpider.ALREADY_SCRAPED_LISTINGS)
+        except FileNotFoundError as e:
+            print(e)
+        try:
+            with open('cordinates.json', 'r') as file:
+                coordinates = json.load(file)
+                print("cord", len(coordinates))
+                if coordinates is not None:
+                    random.shuffle(coordinates)
+                    return [scrapy.FormRequest(
+                        url.format(cord["ne_lat"], cord["ne_lng"], cord["sw_lat"], cord["sw_lng"], ZOOM_LEVEL,
+                                   ZOOM_LEVEL)) for url in
+                        MAP_URL_TEMPLATES for
+                        cord in
+                        coordinates]
+        except FileNotFoundError:
+            print("The file was not found")
+        except json.JSONDecodeError:
+            print("Error decoding JSON")
+
+    async def parse(self, response: Response, **kwargs):
+        """
+        Parse method for extracting listings from the Airbnb search results page.
+
+        Args:
+            response (Response): The response object from the request.
+
+        Yields:
+            Request: A request object for each listing's details page.
+        """
+        if response.status != 200:
+            return
+        script_tag = response.css(f'script#{SCRIPT_TAG}')
+        script_inner_text = script_tag.css('script::text').get()
+        script_json = json.loads(script_inner_text)
+        results = self._parse_listings_json(script_json)
+        current_url = response.url
+
+        if UpdateListingSpider.next_page_cursors is None:
+            UpdateListingSpider.next_page_cursors = self._get_cursors(script_json)
+        for result in results:
+            try:
+                listing = result.get("listing", {})
+                listing_id: str = listing.get("id")
+                print("listing_id", listing_id)
+                title = listing.get("title", "")
+                name = listing.get("name", "")
+                coordinate = listing.get("coordinate", "")
+                latitude = coordinate.get("latitude", "")
+                longitude = coordinate.get("longitude", "")
+                room_type = listing.get("roomTypeCategory", "")
+                page_url = API_CALL.format(n(t("StayListing", listing_id)))
+            except Exception as e:
+                print(e)
+
+            airbnb_params = {
+                "airbnb_listing_id": listing_id,
+                "title": title,
+                "name": name,
+                "latitude": latitude,
+                "longitude": longitude,
+                "room_type": room_type
+            }
+
+            if listing_id is not None and listing_id not in UpdateListingSpider.ALREADY_SCRAPED_LISTINGS:
+                try:
+                    yield Request(url=page_url, callback=self.handle_listing,
+                                  headers={'X-Airbnb-Api-Key': API_KEY},
+                                  meta={'airbnb_params': airbnb_params})
+                except Exception as e:
+                    print("exception", e.args[0])
+            else:
+                print("duplicate", listing_id)
+        if len(UpdateListingSpider.next_page_cursors) != 0:
+            cursor_id = UpdateListingSpider.next_page_cursors.pop()
+            next_url = f'{current_url}&cursor={cursor_id}'
+            yield response.follow(next_url, callback=self.parse)
+
+    @staticmethod
+    def handle_listing(response):
+        """
+        Parse method for extracting details from an Airbnb listing page.
+
+        Args:
+            response (Response): The response object from the request.
+
+         Yields:
+            AirBnBListingItem: An AirBnBListingItem object containing details of an Airbnb listing.
+        """
+        airbnb_params = response.meta.get('airbnb_params', {})
+        listing_item = ExpandedAirBnBListingItem(
+            airbnb_listing_id=airbnb_params.get('airbnb_listing_id'),
+            title=airbnb_params.get('title'),
+            name=airbnb_params.get('name'),
+            latitude=airbnb_params.get('latitude'),
+            longitude=airbnb_params.get('longitude'),
+            room_type=airbnb_params.get('room_type')
+        )
+
+        # script_tag = response.css(f'script#{SCRIPT_TAG}')
+        # script_inner_text = script_tag.css('script::text').get()
+        script_tag_json = json.loads(response.text)
+        location = ""
+        person_capacity = ""
+        try:
+            metadata = UpdateListingSpider._get_metadata_from_json(script_tag_json)
+            if metadata is not None:
+                sharing_config = metadata.get("sharingConfig", {})
+                location = sharing_config.get("location", "")
+                person_capacity = sharing_config.get("personCapacity", "")
+
+        except Exception as e:
+            print(e)
+        listing_item['location'] = location
+        listing_item['person_capacity'] = person_capacity
+        registration_number = "Not Found"
+        number_of_beds = ""
+        number_of_baths = ""
+        try:
+            sections = UpdateListingSpider._parse_listing_json(script_tag_json)
+            for section in sections:
+                if section.get("sectionComponentType") == "PDP_DESCRIPTION_MODAL":
+                    items = section.get("section", {}).get("items", [])
+                    for item in items:
+                        if item.get("title") == "Registration number":
+                            registration_number = item.get("html", "Not Found").get(
+                                "htmlText", "Not Found")
+                            print("regis", registration_number)
+                if section.get("sectionComponentType") == "AVAILABILITY_CALENDAR_DEFAULT":
+                    items = section.get("section", {}).get("descriptionItems", [])
+                    for item in items:
+                        title = item.get("title", "")
+                        if "bed" in title:
+                            number_of_beds = title
+                        if "bath" in title:
+                            number_of_baths = title
+        except Exception as e:
+            print("Exception occurred:", e)
+
+        listing_item["beds"] = number_of_beds
+        listing_item["baths"] = number_of_baths
+        listing_item["registration_number"] = registration_number
+        yield listing_item
+
+    @staticmethod
+    def _safe_get(data: dict, *keys, default=None):
+        """
+        Safely retrieve a nested value from a dictionary.
+
+        Args:
+            data (dict): The dictionary to retrieve the value from.
+            keys (tuple): The keys to navigate the nested structure.
+            default: The default value to return if the keys are not found.
+
+        Returns:
+            The value at the specified nested key or the default value.
+        """
+        try:
+            for key in keys:
+                data = data[key]
+            return data
+        except KeyError or TypeError:
+            print("data", data, keys)
+            return default
+
+    @staticmethod
+    def _parse_listings_json(listings_json):
+        """
+        Parse method for extracting listing information from a JSON object.
+
+        Args:
+            listings_json (dict): The JSON object containing the listings' information.s
+
+        Returns:
+            list: A list of listings extracted from the JSON object.
+        """
+        return UpdateListingSpider._safe_get(listings_json,
+                                             "niobeMinimalClientData", 0, 1, "data", "presentation", "staysSearch",
+                                             "results", "searchResults", default=[])
+
+    @staticmethod
+    def _parse_listing_json(listing_json):
+        """
+        Parse method for extracting listing details from a JSON object.
+
+        Args:
+            listing_json (dict): The JSON object containing the listing details.
+
+        Returns:
+            list: A list of listing details extracted from the JSON object.
+        """
+        return UpdateListingSpider._safe_get(listing_json, "data", "presentation",
+                                             "stayProductDetailPage",
+                                             "sections", "sections", default=[])
+
+    @staticmethod
+    def _get_metadata_from_json(listing_json: dict):
+        return UpdateListingSpider._safe_get(listing_json, "data", "presentation",
+                                             "stayProductDetailPage",
+                                             "sections", "metadata", default=[])
+
+    @staticmethod
+    def _get_cursors(script_json):
+        """
+        Parse method for extracting pagination cursors from a JSON object.
+
+        Args:
+            script_json (dict): The JSON object containing the pagination information.
+
+        Returns:
+            list: A list of pagination cursors extracted from the JSON object.
+        """
+        return UpdateListingSpider._safe_get(script_json,
+                                             "niobeMinimalClientData", 0, 1, "data", "presentation", "staysSearch",
+                                             "results", "paginationInfo", "pageCursors", default=[])
