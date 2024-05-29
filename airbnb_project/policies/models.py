33d5c0a01640d1249af433f775d49ddbde0d7b9b
Nam Anh Mai <maithenamanh@gmail.com>
2024-05-29 00:17:20 -0700

    Changed policy description field from text to char

--- airbnb_project/policies/models.py
+++ airbnb_project/policies/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Policy(models.Model):
   name = models.CharField(max_length=100)
-  description = models.TextField()
+  description = models.CharField(max_length=100)
 
   def __str__(self):
     return self.policy_name
