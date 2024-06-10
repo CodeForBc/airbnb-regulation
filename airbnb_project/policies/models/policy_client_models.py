5d4c3349e4444e3cdee68e8228cd248609369e41
Nam Anh Mai <maithenamanh@gmail.com>
2024-06-09 17:51:03 -0700

    Create model for Policy Client to communicate with different Policy models

---
+++ airbnb_project/policies/models/policy_client_models.py
@@ -0,0 +1,61 @@
+from .policy_models import Policy
+
+
+class PolicyClient:
+    """The policy results contain the policy and result of whether it is violated (bool)
+
+    Example:
+    [
+      {"PolicyA" : True},
+      {"PolicyB": False}
+    ]
+    """
+
+    def __init__(self, policy: Policy) -> None:
+        """Retrieve results from running a policy
+
+        Attribute:
+          policy (Policy) : a method in Policy object
+        """
+
+        self.violation_results = []
+        self._policy = policy
+
+    @property
+    def policy(self) -> Policy:
+        """Reference to the Policy object"""
+        return self._policy
+
+    @policy.setter
+    def policy(self, policy: Policy) -> None:
+        """Setter method to change Policy object at runtime"""
+        self._policy = policy
+
+    def get_violation_result(self) -> None:
+        """Run evaluate method in a Policy object
+        and append policy name and the result to list of violations
+        """
+
+        # Use method name (str) in result
+        policy_name = self._policy.get_policy_name()
+
+        # Result includes policy name as key, and the return value of that policy as value
+        policy_result = self._policy.evaluate()
+
+        # Only include violation results if policy name does not exist
+        if not any(policy_name in result for result in self.violation_results):
+            self.violation_results.append({policy_name: policy_result})
+            return
+        else:
+            raise ValueError(f"{policy_name} already existed in violation results")
+
+    def get_all_violation_results(self) -> list[dict[str, bool]]:
+        """Get all violation results with policy name and the result
+
+        Example:
+        [
+          {'ValidRegistrationNumberPolicy': True},
+          {'UniqueRegistrationNumberPolicy': False}
+        ]
+        """
+        return self.violation_results.copy()
