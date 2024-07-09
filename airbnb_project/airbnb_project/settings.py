4a09ee7c6735bdc46838a0e28e928f4c578a17a9
Nam Anh Mai <maithenamanh@gmail.com>
2024-07-08 17:59:41 -0700

    Changed settings and created test settings to use postgresql db

--- airbnb_project/airbnb_project/settings.py
+++ airbnb_project/airbnb_project/settings.py
@@ -80,8 +80,7 @@ WSGI_APPLICATION = 'airbnb_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
     }
 }
 
