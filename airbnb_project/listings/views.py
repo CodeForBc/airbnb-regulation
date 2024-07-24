06e4adfbaedacc37764b3de63807b0caef735404
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-07-23 22:56:23 -0700

    add harvester request handler

--- airbnb_project/listings/views.py
+++ airbnb_project/listings/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
+from listings.harvestor_app.harvester.spiders.update_listing_spider import UpdateListingSpider
+from listings.harvestor_app.harvester.test_settings import get_scrapy_settings
 
-# Create your views here.
+
+def scrapy_background_process():
+    print("Scrapy process started")
+    runner = CrawlerRunner(settings=get_scrapy_settings())
+    d = runner.crawl(UpdateListingSpider)
+    d.addBoth(lambda _: reactor.stop())
+    reactor.run()
+    print("Scrapy process done")
+
+
+def scrape_listings(request):
+    scrapy_background_process()
+    return HttpResponse("process started")
