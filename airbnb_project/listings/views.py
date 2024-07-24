4ba37991822151e91abd6ecca12f807e2a690a31
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-07-23 23:34:01 -0700

    fix module import issue

--- airbnb_project/listings/views.py
+++ airbnb_project/listings/views.py
@@ -1,14 +1,14 @@
 from django.http import HttpResponse
 from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
-from listings.harvestor_app.harvester.spiders.update_listing_spider import UpdateListingSpider
-from listings.harvestor_app.harvester.test_settings import get_scrapy_settings
+from listings.harvester_app.harvester.spiders.listings_spider import ListingsSpider
+from listings.harvester_app.harvester.test_settings import get_scrapy_settings
 
 
 def scrapy_background_process():
     print("Scrapy process started")
     runner = CrawlerRunner(settings=get_scrapy_settings())
-    d = runner.crawl(UpdateListingSpider)
+    d = runner.crawl(ListingsSpider)
     d.addBoth(lambda _: reactor.stop())
     reactor.run()
     print("Scrapy process done")
