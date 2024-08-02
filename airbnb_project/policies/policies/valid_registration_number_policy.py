eac9b18e8e798371e3d65d1d54df8125d2281d8d
Nam Anh Mai <maithenamanh@gmail.com>
2024-08-02 15:12:39 -0700

    Changed reg num policy to evaluate str with get_evaluation_result()

--- airbnb_project/policies/policies/valid_registration_number_policy.py
+++ airbnb_project/policies/policies/valid_registration_number_policy.py
@@ -1,21 +1,17 @@
-from django.db import models
 from ..models.policy_model import Policy
-
 import re
 
 class ValidRegistrationNumberPolicy(Policy):
-  registration_number = models.CharField(max_length=20)
-
-  def is_valid_registration_number(self) -> bool:
-    if self.registration_number is None or self.registration_number == "":
+  def is_valid_registration_number(self, registration_number: str) -> bool:
+    if not registration_number:
       return False
     
     valid_registration_pattern = re.compile(r"^[0-9]{2}-[0-9]{6}$")
     START_YEAR = 13
     END_YEAR = 24
 
-    if valid_registration_pattern.match(self.registration_number):
-        registration_year = int(self.registration_number[0:2])
+    if valid_registration_pattern.match(registration_number):
+        registration_year = int(registration_number[0:2])
         if registration_year > END_YEAR or registration_year < START_YEAR:
             return False
         else:
