1dc4abc87710f8867d2765ac8206aa6b487eadc8
Nam Anh Mai <maithenamanh@gmail.com>
2024-08-02 22:03:12 -0700

    Changed init method to contain all attributes from base policy model

--- airbnb_project/policies/policies/issued_registration_number_policy.py
+++ airbnb_project/policies/policies/issued_registration_number_policy.py
@@ -2,11 +2,23 @@ from ..models.policy_model import Policy
 from ..services.business_licence_client import BusinessLicenceClient
 
 class IssuedRegistrationNumberPolicy(Policy):
-  def __init__(self, name, description) -> None:
-    super().__init__(name, description)
+  """
+  Policy to check if registration number is issued in Business Licences dataset in Vancouver Open Data Portal.
+  """
+  def __init__(self, *args, **kwargs) -> None:
+    super().__init__(*args, **kwargs)
     self.business_licence_client = BusinessLicenceClient()
 
-  def get_evaluation_result(self, registration_number: str) -> bool:
+  def evaluate(self, registration_number: str) -> bool:
+    """
+    Evaluate the policy on the given registration number.
+
+    Args:
+        registration_number (str): The registration number to evaluate.
+
+    Returns:
+        bool: True if the registration number is valid, False otherwise.
+    """
     result = self.business_licence_client.get_licence_status(registration_number)
     if result == "Issued":
       return True
