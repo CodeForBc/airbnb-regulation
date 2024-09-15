8bb0aaffc58d5c34973299d8827941fd9ad977b3
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-09-15 13:58:16 -0700

    get airbnb key from env

--- airbnb_project/listings/harvester_app/harvester/custom_settings.py
+++ airbnb_project/listings/harvester_app/harvester/custom_settings.py
@@ -1,17 +1,25 @@
 import os
 
+from dotenv import dotenv_values
+
 """
 Files contains custom settings for the harvester and values related to AirBnB.
 """
 
 
 def get_scrapy_settings():
+    # load the env file
+    config = dotenv_values(".env")
+
+    # get the airbnb api key
+    AIRBNB_PUBLIC_API_KEY = config.get("AIRBNB_PUBLIC_API_KEY", "")
+
     current_directory = os.path.dirname(os.path.abspath(__file__))
     # Construct the absolute path to coordinates.json
     coordinates_file_path = os.path.join(current_directory, 'spiders/listings.csv')
     settings_dict = {
         # Values related to AirBnb
-        'AIRBNB_PUBLIC_API_KEY': "",
+        'AIRBNB_PUBLIC_API_KEY': AIRBNB_PUBLIC_API_KEY,
         'AIRBNB_LISTING_API_URL': "https://www.airbnb.ca/api/v3/StaysPdpSections/08e3ad2e3d75c9bede923485718ff2e7f6efe2ca1febb5192d78c51e17e8b4ca?operationName=StaysPdpSections&locale=en-CA&currency=CAD&variables=%7B%22id%22%3A%22{}%22%2C%22pdpSectionsRequest%22%3A%7B%22adults%22%3A%221%22%2C%22layouts%22%3A%5B%22SINGLE_COLUMN%22%5D%7D%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%2237d7cbb631196506c3990783fe194d81432d0fbf7362c668e547bb6475e71b37%22%7D%7D",
         'AIRBNB_SCRIPT_TAG': "data-deferred-state-0",
         'ZOOM_LEVEL': 15.4,
