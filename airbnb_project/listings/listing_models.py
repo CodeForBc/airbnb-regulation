ffb9122c3fdb3e5acc177806fbf7ba1fc4955b79
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-11-11 00:09:19 -0800

    refactor(listings): change column type of numerical value to float(baths,bed) and Integer(person capacity)

--- airbnb_project/listings/listing_models.py
+++ airbnb_project/listings/listing_models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
+
 class Listing(models.Model):
     airbnb_listing_id = models.TextField(blank=False, null=False)
     name = models.TextField(null=True, blank=True)
-    baths = models.TextField(null=True, blank=True)
-    beds = models.TextField(null=True, blank=True)
+    baths = models.FloatField(null=True, blank=True)
+    beds = models.FloatField(null=True, blank=True)
     latitude = models.TextField(null=True, blank=True)
     location = models.TextField(null=True, blank=True)
     longitude = models.TextField(null=True, blank=True)
-    person_capacity = models.TextField(null=True, blank=True)
+    person_capacity = models.IntegerField(null=True, blank=True)
     registration_number = models.TextField(null=True, blank=True)
     room_type = models.TextField(null=True, blank=True)
     title = models.TextField(null=True, blank=True)
