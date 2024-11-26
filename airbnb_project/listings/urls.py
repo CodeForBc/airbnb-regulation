43eb4b2a81c6bbd45fe457cfb24f8b48a5244355
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-11-25 17:13:49 -0800

    feat(celery): add celery task urls

--- airbnb_project/listings/urls.py
+++ airbnb_project/listings/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("harvest-listings/", views.harvest_listings, name="harvest_listings"),
+    path("with-celery/", views.with_celery, name="with_celery"),
+    path("without-celery/", views.without_celery, name="without_celery"),
 ]
