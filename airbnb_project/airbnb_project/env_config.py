ceee82447e8f08fb4c5d5ab30a476b450056e9f9
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-12-06 18:27:29 -0800

    refactor(env-config): change file name and move custom exceptions to its own file

--- airbnb_project/airbnb_project/env_config.py
+++ airbnb_project/airbnb_project/env_config.py
@@ -1,13 +1,10 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
+from airbnb_project.airbnb_project.exceptions import EnvConfigException
+
 # Load environment variables from a .env file
 load_dotenv(find_dotenv())
-
-
-class ConfigError(Exception):
-    """Custom exception for configuration errors."""
-    pass
 
 
 def get_env_variable(var_name: str) -> str:
@@ -25,9 +22,9 @@ def get_env_variable(var_name: str) -> str:
     try:
         value = os.environ[var_name]
         if not value:  # Check if the value is empty
-            raise ConfigError(f"Missing required environment variable: {var_name}")
+            raise EnvConfigException(f"Missing required environment variable: {var_name}")
     except KeyError:
-        raise ConfigError(f"Missing required environment variable: {var_name}")
+        raise EnvConfigException(f"Missing required environment variable: {var_name}")
 
     return value  # Return the value if it exists
 
@@ -38,4 +35,5 @@ ENV_VARIABLES: dict[str, str] = {
     'POSTGRES_USER': get_env_variable('POSTGRES_USER'),
     'POSTGRES_PASSWORD': get_env_variable('POSTGRES_PASSWORD'),
     'POSTGRES_HOST_PORT': get_env_variable('POSTGRES_HOST_PORT'),
+    'DJANGO_DEBUG': get_env_variable('DJANGO_DEBUG')
 }
