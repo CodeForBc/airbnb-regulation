e9a00004d2ed6f678a351136ff6439170c84e02c
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-12-15 18:56:29 -0800

    doc(celery): add doc strings to file

--- airbnb_project/listings/tasks.py
+++ airbnb_project/listings/tasks.py
@@ -1,5 +1,5 @@
 from celery import shared_task
-from scrapy.crawler import CrawlerRunner, CrawlerProcess
+from scrapy.crawler import CrawlerProcess
 from scrapy.utils.log import configure_logging
 from listings.harvester_app.harvester.spiders.listings_spider import ListingsSpider
 from listings.harvester_app.harvester.harvester_settings import get_harvester_settings
@@ -12,7 +12,15 @@ configure_logging()
 
 def run_spider():
     """
-    Run the spider and return a deferred that will fire when the spider completes.
+    Run the Scrapy spider for harvesting listings.
+
+    This function initializes a Scrapy CrawlerProcess with the required settings,
+    schedules the `ListingsSpider` to run, and starts the crawling process.
+    The function runs in non-blocking mode with `stop_after_crawl=False` to keep
+    the process active after the spider completes.
+
+    Returns:
+        None
     """
     runner = CrawlerProcess(settings=get_harvester_settings())
     runner.crawl(ListingsSpider)
@@ -22,7 +30,17 @@ def run_spider():
 @shared_task(bind=True)
 def run_harvest_task(self):
     """
-    Celery task to run the Scrapy spider.
+    Celery task to trigger the Scrapy spider for harvesting listings.
+
+    This task runs the `run_spider` function to start the spider process.
+    It handles any exceptions during the spider execution and logs success
+    or error messages.
+
+    Args:
+        self: Reference to the current Celery task instance.
+
+    Returns:
+        None
     """
     try:
         # Run the spider and wait for it to complete
