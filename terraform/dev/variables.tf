2738bdf75d2c1d83e7f9222d7bd64bbb5c7f06ad
Mangat Toor <immangat@amazon.com>
2025-06-02 18:09:15 -0700

    ci: Implement CI/CD pipeline with Terraform
    Adds a GitHub Actions workflow to automate the deployment process.
    
    - Provisions infrastructure on AWS using Terraform.
    - Deploys the application to an EC2 instance.
    - Includes secret validation and a cron job setup script.

---
+++ terraform/dev/variables.tf
@@ -0,0 +1,7 @@
+variable "bucket_name_dev" {default = ""}
+variable "bucket_key_dev" {default = ""}
+variable "key_name_dev" {default = ""}
+variable "public_key_path_dev" {default = ""}
+variable "db_name_dev" { default = "" }
+variable "db_user_dev" { default = "" }
+variable "db_password_dev" {default = ""}
